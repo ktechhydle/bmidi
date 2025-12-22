adec000809220d5a47c0bf6e920f807c0d447fb2
ktechhydle <kellerhydle@gmail.com>
2025-12-21 19:17:42 -0700

    Example Updates
    -finish drum set example

--- examples/drum_set/main.py
+++ examples/drum_set/main.py
@@ -14,16 +14,19 @@ def initialize():
 initialize()
 
 import bpy
-import math
 from src.instrument import Instrument, append_instrument
 
-offset = 12
+offset = 12 # the midi track is offset by 12 due to the way it was created
 
-cowbell = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Cowbell_Stick", "rotation_euler.x", 45, note=68 - offset)
-tom_drum = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Tom_Stick", "rotation_euler.x", 45, note=62 - offset)
+cowbell = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Cowbell_Stick", "rotation_euler.x", 90, note=68 - offset)
+tom_drum = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Tom_Stick", "rotation_euler.x", 90, note=62 - offset)
+snare_drum = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Snare_Stick", "rotation_euler.x", 90, note=50 - offset)
+kick_drum = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Kick_Stick", "rotation_euler.x", 90, note=47 - offset)
 
 bpy.context.scene.frame_set(-1)
 bpy.app.handlers.frame_change_pre.clear()
 
 append_instrument(cowbell)
 append_instrument(tom_drum)
+append_instrument(snare_drum)
+append_instrument(kick_drum)
