0a7ce53e9cc318aeb0b5aea6b401f926cf953fb2
ktechhydle <kellerhydle@gmail.com>
2025-12-23 22:14:48 -0700

    Example Updates
    -add drum "hit" motion to script

--- examples/drum_set/main.py
+++ examples/drum_set/main.py
@@ -18,15 +18,23 @@ from src.instrument import Instrument, append_instrument
 
 offset = 12 # the midi track is offset by 12 due to the way it was created
 
-cowbell = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Cowbell_Stick", "rotation_euler.x", 90, note=68 - offset)
-tom_drum = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Tom_Stick", "rotation_euler.x", 90, note=62 - offset)
-snare_drum = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Snare_Stick", "rotation_euler.x", 90, note=50 - offset)
-kick_drum = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Kick_Stick", "rotation_euler.x", 90, note=47 - offset)
+cowbell_hammer = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Cowbell_Stick", "rotation_euler.x", 90, note=68 - offset)
+cowbell = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Cowbell", "rotation_euler.x", 3, note=68 - offset)
+tom_drum_hammer = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Tom_Stick", "rotation_euler.x", 90, note=62 - offset)
+tom_drum = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Tom", "location.z", -3, note=62 - offset)
+snare_drum_hammer = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Snare_Stick", "rotation_euler.x", 90, note=50 - offset)
+snare_drum = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Snare", "location.z", -3, note=50 - offset)
+kick_drum_hammer = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Kick_Stick", "rotation_euler.x", 90, note=47 - offset)
+kick_drum = Instrument("/home/keller/mpsoftware/bmidi/examples/drum_set/track.mid", "Kick", "location.x", -3, note=47 - offset)
 
 bpy.context.scene.frame_set(-1)
 bpy.app.handlers.frame_change_pre.clear()
 
+append_instrument(cowbell_hammer)
 append_instrument(cowbell)
+append_instrument(tom_drum_hammer)
 append_instrument(tom_drum)
+append_instrument(snare_drum_hammer)
 append_instrument(snare_drum)
+append_instrument(kick_drum_hammer)
 append_instrument(kick_drum)
