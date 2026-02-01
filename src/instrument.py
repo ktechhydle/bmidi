6c0546045c32dbf837f6ba5faa1a4dae6fc2609e
ktechhydle <kellerhydle@gmail.com>
2026-02-01 09:13:51 -0700

    Instrument Updates
    -add robotic instrument (WIP)

--- src/instrument.py
+++ src/instrument.py
@@ -296,3 +296,110 @@ class MovementInstrument(Instrument):
                 data_path=keyframe_prop,
                 frame=end_frame + 1
             )
+
+class RoboticInstrument(Instrument):
+    """
+    Represents a robotic-arm-like instrument that reaches and hits a note at a specified position
+
+    `bone_name`: the bone to control
+    `rest_position`: the armature's resting position
+    `reach_position`: the armature's reach position to hit the note
+    `note`: what pitch (numbers 1-127) controls the object, leaving this kwarg blank will result in the object moving based on all the notes in the midi file
+    `channel`: what channel (numbers 0-15) controls the object, leaving this kwarg blank will result in the object moving based on all the channels in the midi file
+    `affected_object`: an object (if any) that might be affected by this instrument, where `tuple[str, str, float]` is the object's name, property, and movement amount
+
+    ## Example:
+
+    ```python
+    snare_drum_arm = RoboticInstrument(
+        "track.mid", # midi file
+        "Bone1", # bone to control
+        (0, 0, 0), # initial position
+        (1, 1, 1), # final position
+        note=25, # what pitch controls the object
+        channel=9, # what channel controls the object
+        affected_object=("Snare", "location.z", -0.1), # what object is affected by this object
+    )
+    snare_drum_arm.generate_keyframes() # generate the keyframes
+    ```
+    """
+    def __init__(
+        self,
+        midi_file: str,
+        armature_name: str,
+        bone_name: str,
+        rest_position: tuple[float, float, float],
+        reach_position: tuple[float, float, float],
+        note: int | None = None,
+        channel: int | None = None,
+        affected_object: tuple[str, str, float] | None = None,
+    ):
+        super().__init__(midi_file, note, channel)
+
+        self.armature = bpy.data.objects[armature_name]
+        self.pose_bone = self.armature.pose.bones.get(bone_name)
+        self.rest_position = rest_position
+        self.reach_position = reach_position
+
+        if affected_object is not None:
+            self.affected_object = bpy.data.objects[affected_object[0]]
+            self.affected_object_property = affected_object[1]
+            self.affected_object_movement_amount = affected_object[2]
+
+            self.affected_object.animation_data_clear()
+
+        self.armature.animation_data_clear()
+
+    def generate_keyframes(self):
+        fps = bpy.context.scene.render.fps
+        arm = self.armature
+        bone = self.pose_bone
+
+        for e in self.events():
+            start_frame = e["start"] * fps
+            duration = 0.08 * fps
+            pullback_scale = 1 + (1 - e["velocity"]) * 1.5
+
+            bone.location = self.rest_position
+            arm.keyframe_insert(
+                data_path=f'pose.bones["{bone.name}"].location',
+                frame=start_frame - (duration * pullback_scale)
+            )
+
+            # hit
+            bone.location = self.reach_position
+            arm.keyframe_insert(
+                data_path=f'pose.bones["{bone.name}"].location',
+                frame=start_frame
+            )
+
+            # if hasattr(self, "affected_object"):
+            #     affected_prop = self.affected_object_property
+            #     affected_keyframe_prop = affected_prop.split(".")[0]
+            #     prop_root, prop_axis = self.affected_object_property.split(".")
+            #     og_position = getattr(getattr(self.affected_object, prop_root), prop_axis)
+
+            #     exec(f"self.affected_object.{self.affected_object_property} = og_position")
+            #     self.affected_object.keyframe_insert(
+            #         data_path=affected_keyframe_prop,
+            #         frame=start_frame - 1
+            #     )
+
+            #     exec(f"self.affected_object.{self.affected_object_property} = og_position + self.affected_object_movement_amount")
+            #     self.affected_object.keyframe_insert(
+            #         data_path=affected_keyframe_prop,
+            #         frame=start_frame
+            #     )
+
+            #     exec(f"self.affected_object.{self.affected_object_property} = og_position")
+            #     self.affected_object.keyframe_insert(
+            #         data_path=affected_keyframe_prop,
+            #         frame=start_frame + duration
+            #     )
+
+            # return
+            bone.location = self.rest_position
+            arm.keyframe_insert(
+                data_path=f'pose.bones["{bone.name}"].location',
+                frame=start_frame + (duration * pullback_scale)
+            )
